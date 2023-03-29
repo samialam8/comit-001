98e28f3f4363b54594e5fac1f5c31124fa4274c9
Sami Alam <sami.alam10@gmail.com>
2023-03-29 16:39:44 -0600

    try with resources used means optimization done

--- src/main/java/com/comit/course/_jdbc/_01_JdbcStmtList.java
+++ src/main/java/com/comit/course/_jdbc/_01_JdbcStmtList.java
@@ -1,8 +1,11 @@
 package com.comit.course._jdbc;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class _01_JdbcStmtList {
 
@@ -12,13 +15,32 @@ public class _01_JdbcStmtList {
 		String dbUsername = "root";
 		String dbPassword = "Mysql123@";
 		
-		try {
-			Connection con = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+		String sql = "SELECT * FROM USER";
+		
+		try(Connection con = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);) {
+			
+			
+			Statement st = con.createStatement();
+			
+			ResultSet rs = st.executeQuery(sql);
+			
+			while (rs.next()) {
+				int idUser = rs.getInt("ID_USER");
+				String firstName = rs.getString("FIRST_NAME");
+				String lastName = rs.getString("LAST_NAME");
+				String userName = rs.getString("USERNAME");
+				String password = rs.getString("PASSWORD");
+				Date birth = rs.getDate("BIRTH");
+				String status = rs.getString("STATUS");
+				
+				System.out.println(idUser + " " + firstName + " " + lastName + " " +  userName + " " + password + " " + birth + " " + status);
+			}
 			
 			System.out.println("Connection successful!");
 		} catch (SQLException e) {
 			System.err.println("Error while connecting to the DB");
 			e.printStackTrace();
-		}
+		} 
+		
 	}
 }
